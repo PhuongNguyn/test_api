4433f7df072b04d78a13b39021a51d36cebecf95
Phuong <zinzin.vung@gmail.com>
2023-06-18 23:02:15 +0700

    first commit

---
+++ furniture.js
@@ -0,0 +1,222 @@
+export const furniture = [
+    {
+        "Id": 24565,
+        "Name": "Bàn sofa cũ  SP019795",
+        "NumberCard": "SP019795",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/6_2023/mvina-4941505mvina-3.jpg",
+        "catUrl": "sofa-salon-cu",
+        "FriendlyUrl": "ban-sofa-cu-sp019795",
+        "OnHand": 1
+    },
+    {
+        "Id": 24545,
+        "Name": "Tủ quần áo cũ  SP019773",
+        "NumberCard": "SP019773",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/6_2023/mvina-2.jpg",
+        "catUrl": "tu-quan-ao-cu",
+        "FriendlyUrl": "tu-quan-ao-cu-sp019773",
+        "OnHand": 1
+    },
+    {
+        "Id": 24544,
+        "Name": "Tủ commosst cũ SP019774",
+        "NumberCard": "SP019774",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/6_2023/mvina-3.jpg",
+        "catUrl": "tu-ngan-keo-cu",
+        "FriendlyUrl": "tu-commosst-cu-sp019774",
+        "OnHand": 1
+    },
+    {
+        "Id": 24543,
+        "Name": "Bộ sofa cũ SP019775",
+        "NumberCard": "SP019775",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/6_2023/mvina-4.jpg",
+        "catUrl": "sofa-salon-cu",
+        "FriendlyUrl": "bo-sofa-cu-sp019775",
+        "OnHand": 1
+    },
+    {
+        "Id": 24537,
+        "Name": "Giường gỗ tự nhiên cũ  SP019763",
+        "NumberCard": "SP019763",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/5_2023/mvina-1024524mvina-5.jpg",
+        "catUrl": "giuong-cu",
+        "FriendlyUrl": "giuong-go-tu-nhien-cu-sp019763",
+        "OnHand": 1
+    },
+    {
+        "Id": 24536,
+        "Name": "Giường gỗ tự nhiên cũ  SP019764",
+        "NumberCard": "SP019764",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/5_2023/mvina-910832mvina-4.jpg",
+        "catUrl": "giuong-cu",
+        "FriendlyUrl": "giuong-go-tu-nhien-cu-sp019764",
+        "OnHand": 1
+    },
+    {
+        "Id": 24531,
+        "Name": "Bộ sofa cũ SP019761",
+        "NumberCard": "SP019761",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/5_2023/mvina-5654607mvina-9.jpg",
+        "catUrl": "sofa-salon-cu",
+        "FriendlyUrl": "bo-sofa-cu-sp019761",
+        "OnHand": 1
+    },
+    {
+        "Id": 24529,
+        "Name": "Bộ bàn ăn 6 ghế cũ SP019752",
+        "NumberCard": "SP019752",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/5_2023/mvina-1812997mvina-0.jpg",
+        "catUrl": "ban-ghe-an-cu",
+        "FriendlyUrl": "bo-ban-an-6-ghe-cu-sp019752",
+        "OnHand": 1
+    },
+    {
+        "Id": 24528,
+        "Name": "Tủ quần áo cũ  SP019753",
+        "NumberCard": "SP019753",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/5_2023/mvina-419420mvina-8.jpg",
+        "catUrl": "tu-quan-ao-cu",
+        "FriendlyUrl": "tu-quan-ao-cu-sp019753",
+        "OnHand": 1
+    },
+    {
+        "Id": 24527,
+        "Name": "Bàn phấn  cũ SP019754",
+        "NumberCard": "SP019754",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/5_2023/mvina-1928792mvina-1.jpg",
+        "catUrl": "ban-phan-ban-trang-diem-cu",
+        "FriendlyUrl": "ban-phan-cu-sp019754",
+        "OnHand": 1
+    },
+    {
+        "Id": 24514,
+        "Name": "Kệ sách  cũ SP019744",
+        "NumberCard": "SP019744",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/5_2023/mvina-5949917mvina-6.jpg",
+        "catUrl": "tu-ke-sach-cu",
+        "FriendlyUrl": "ke-sach-cu-sp019744",
+        "OnHand": 1
+    },
+    {
+        "Id": 24502,
+        "Name": "Tủ tivi cũ  SP019723",
+        "NumberCard": "SP019723",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/5_2023/mvina-1833806mvina-19.jpg",
+        "catUrl": "tu-ke-tivi-cu",
+        "FriendlyUrl": "tu-tivi-cu-sp019723",
+        "OnHand": 1
+    },
+    {
+        "Id": 24501,
+        "Name": "Tủ tivi cũ  SP019724",
+        "NumberCard": "SP019724",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/5_2023/mvina-1938231mvina-20.jpg",
+        "catUrl": "tu-ke-tivi-cu",
+        "FriendlyUrl": "tu-tivi-cu-sp019724",
+        "OnHand": 1
+    },
+    {
+        "Id": 24500,
+        "Name": "Giường  gỗ MDF cũ SP019725",
+        "NumberCard": "SP019725",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/5_2023/mvina-1612731mvina-14.jpg",
+        "catUrl": "giuong-cu",
+        "FriendlyUrl": "giuong-go-mdf-cu-sp019725",
+        "OnHand": 1
+    },
+    {
+        "Id": 24497,
+        "Name": "Bồn rửa tay cũ SP019728",
+        "NumberCard": "SP019728",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/5_2023/mvina-3659791mvina-24.jpg",
+        "catUrl": "bon-chau-rua-cu",
+        "FriendlyUrl": "bon-rua-tay-cu-sp019728",
+        "OnHand": 2
+    },
+    {
+        "Id": 24492,
+        "Name": "Giường  gỗ MDF cũ SP019720",
+        "NumberCard": "SP019720",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/5_2023/mvina-1435778mvina-16.jpg",
+        "catUrl": "giuong-cu",
+        "FriendlyUrl": "giuong-go-mdf-cu-sp019720",
+        "OnHand": 1
+    },
+    {
+        "Id": 24490,
+        "Name": "Kệ bếp trên dưới  cũ SP019722",
+        "NumberCard": "SP019722",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/5_2023/mvina-1653629mvina-18.jpg",
+        "catUrl": "tu-bep-cu",
+        "FriendlyUrl": "ke-bep-tren-duoi-cu-sp019722",
+        "OnHand": 1
+    },
+    {
+        "Id": 24484,
+        "Name": "Tủ kệ trang trí cũ SP019716",
+        "NumberCard": "SP019716",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/5_2023/mvina-1037537mvina-12.jpg",
+        "catUrl": "tu-ke-trung-bay-trang-tri-cu",
+        "FriendlyUrl": "tu-ke-trang-tri-cu-sp019716",
+        "OnHand": 1
+    },
+    {
+        "Id": 24482,
+        "Name": "Nệm bông ép cũ SP019718",
+        "NumberCard": "SP019718",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/5_2023/mvina-1229808mvina-14.jpg",
+        "catUrl": "nem-cu",
+        "FriendlyUrl": "nem-bong-ep-cu-sp019718",
+        "OnHand": 1
+    },
+    {
+        "Id": 24481,
+        "Name": "Nệm bông ép cũ SP019719",
+        "NumberCard": "SP019719",
+        "Price": 0,
+        "PriceOld": 0,
+        "Image": "https://vinasave.com/vnt_upload/product/5_2023/mvina-1322939mvina-15.jpg",
+        "catUrl": "nem-cu",
+        "FriendlyUrl": "nem-bong-ep-cu-sp019719",
+        "OnHand": 1
+    }
+]
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const router = express.Router()
+const {furniture} = require('./furniture')
+const {flowers} = require('./flowers')
+const {travel} = require('./travel')
+
+
+app.get('/furniture', (req, res) => {
+    return res.status(200).json(furniture)
+})
+
+app.get('/flower', (req, res) => {
+    return res.status(200).json(flowers)
+})
+
+app.get('/travel', (req, res) => {
+    return res.status(200).json(travel)
+})
+
+app.listen(3001, () => {
+    console.log("app is listen on http://localhost:" + 3001)
+})
